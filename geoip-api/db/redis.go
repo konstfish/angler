2fbeaafb9757be247c80bae29b8783b7874a38b8
David Fischer <david@konst.fish>
2024-01-06 19:36:32 +0100

    feat(geoip-api): Add basic rate limiting

--- geoip-api/db/redis.go
+++ geoip-api/db/redis.go
@@ -4,12 +4,14 @@ import (
 	"context"
 	"log"
 	"os"
+	"time"
 
 	"github.com/konstfish/angler/geoip-api/controllers"
 	"github.com/redis/go-redis/v9"
 	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
+	"golang.org/x/time/rate"
 )
 
 type RedisClient struct {
@@ -39,6 +41,9 @@ func NewRedisClient() *RedisClient {
 
 func (r *RedisClient) ListenForNewItems(queueName string) {
 	log.Println("Listening for new items in queue", queueName)
+
+	limiter := rate.NewLimiter(rate.Every(time.Minute/50), 50) // 50 requests per minute
+
 	for {
 		result, err := r.Client.BLPop(r.Ctx, 0, queueName).Result()
 		if err != nil {
@@ -53,25 +58,34 @@ func (r *RedisClient) ListenForNewItems(queueName string) {
 		}
 
 		if !exists {
-			geoip, err := controllers.GetIpInfo(result[1])
-			if err != nil {
-				log.Fatal(err)
+			if limiter.Allow() {
+				go processAddress(result[1])
+			} else {
+				log.Println("Rate limit exceeded, requeueing")
+				r.PushToQueue(queueName, result[1])
+				time.Sleep(time.Second * 3)
 			}
-
-			filter := bson.M{"_id": geoip.Address}
-			opts := options.Replace().SetUpsert(true)
-			result, err := geoIpCollection.ReplaceOne(context.TODO(), filter, geoip, opts)
-			if err != nil {
-				log.Fatal(err)
-			}
-
-			log.Println(geoip.Address, result)
 		}
-
 	}
 }
 
-/*func (r *RedisClient) PushToQueue(queueName string, value string) {
+func processAddress(address string) {
+	geoip, err := controllers.GetIpInfo(address)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	filter := bson.M{"_id": geoip.Address}
+	opts := options.Replace().SetUpsert(true)
+	result, err := geoIpCollection.ReplaceOne(context.TODO(), filter, geoip, opts)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	log.Println("Added IP:", geoip.Address, result)
+}
+
+func (r *RedisClient) PushToQueue(queueName string, value string) {
 	log.Println("Pushing to queue", queueName)
 	r.Client.RPush(r.Ctx, queueName, value)
-}*/
+}
