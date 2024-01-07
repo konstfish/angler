aa105b97367a763a16432705a6804e4f9fa1f96f
David Fischer <david@konst.fish>
2024-01-07 22:20:38 +0100

    feat(ingress): Minor changes to path

--- ingress/mappings/mappings.go
+++ ingress/mappings/mappings.go
@@ -12,7 +12,7 @@ func CreateUrlMappings() {
 
 	Router.Use(controllers.Cors())
 
-	v1 := Router.Group("/v1")
+	v1 := Router.Group("/ingress/v1")
 	{
 		v1.POST("/session/:domain", controllers.DomainReferrer(), controllers.PostSession)
 		v1.POST("/event/:domain/session/:sessionId", controllers.DomainReferrer(), controllers.PostEvent)
