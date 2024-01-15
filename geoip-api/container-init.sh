2c20545e8b6751801d983ffd442c106b050b80b7
David Fischer <david@konst.fish>
2024-01-15 22:52:19 +0100

    feat(geoip-api): Update container init script

--- geoip-api/container-init.sh
+++ geoip-api/container-init.sh
@@ -1,8 +1,15 @@
 #!/bin/sh
+set -e
 
 REPO="P3TERX/GeoLite.mmdb"
 FILE="GeoLite2-City.mmdb"
 
-wget https://github.com/P3TERX/GeoLite.mmdb/releases/latest/download/GeoLite2-City.mmdb
+if [ ! -f "$FILE" ] || find "$FILE" -mtime +7 -print | grep -q .; then
+    echo "Redownloading database: $FILE from $REPO"
+    wget -O "$FILE" "https://github.com/$REPO/releases/latest/download/$FILE" || { echo "Download failed"; exit 1; }
+    echo "Download successful"
+else
+    echo "Database is up-to-date"
+fi
 
 exec ./main
